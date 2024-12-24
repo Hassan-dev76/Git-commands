307ce45af6d6f96436afecb88e16430cfd69bfc0
Hassan <connecthassan76@gmail.com>
2024-12-24 15:41:48 +0500

    Add greet function

--- git_text.py
+++ git_text.py
@@ -1,3 +1,11 @@
 print("Hello Git")
 print("This is a second line for precticing git")
 print("This is thirld line of git file")
+
+
+def greet():
+    return "wellcome to the git project"
+
+
+
+greet()
